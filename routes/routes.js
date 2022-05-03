bb20ae403e846b4499566928fb3656113d772a05
adamglang <adam@getlabs.com>
2022-05-02 18:17:50 -0700

    try json instead

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const controller = require('../controllers/controllers');
 
 function routes(app) {
-    app.route('/').get((req, res) => res.text('OK'));
+    app.route('/').get((req, res) => res.json('OK'));
     app.route('/list').get(controller.listDinners)
     app.route('createDinner').post(controller.createDinner)
 }
